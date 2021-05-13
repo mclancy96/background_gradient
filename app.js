c1f684676630d3ceb4d0ef64b4188bd3961c95f8
Mike <mclancy96@gmail.com>
2021-05-13 18:35:12 -0400

    Added app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express"),
+    app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req,res) => {
+	res.render("./index.html", {});
+});
+
+app.listen(process.env.PORT, function(){
+	console.log("Gradient app has started...");
+});
