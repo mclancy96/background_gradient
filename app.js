d27c6b05968a30f8099b6a0adf89f29cbf17bef9
Mike <mclancy96@gmail.com>
2021-05-13 18:46:22 -0400

    Spelling

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req,res) => {
-	res.render("./index.html", {});
+	res.render("./index", {});
 });
 
 app.listen(process.env.PORT, function(){
